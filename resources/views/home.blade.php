a8197e149d7091bc0cc94069b2f8922a0b3f999c
mcsjnn <xiuhan958@gmail.com>
2025-03-27 21:54:11 -0500

    Agregando archivos del proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <h2>Mi página principal</h2>
+      <p class="lead">Esto es un párrafo dando a conocer los aspectos generales de mi página.</p>
+    </div>
+  </main>
+</div>
+
+@endsection
